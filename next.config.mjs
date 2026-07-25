1fb08ba01bba88aaac88a67f33c776544eb47c3f
Vanesa Paco Alvarez <138837245+TechiWriter@users.noreply.github.com>
2026-07-25 18:39:29 -0400

    Update next.config.mjs
    configuración para que Amplify exponga las variables de entorno al servidor.

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,10 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  env: {
+    ANTHROPIC_API_KEY: process.env.ANTHROPIC_API_KEY,
+    NOTION_TOKEN: process.env.NOTION_TOKEN,
+    NOTION_DB_ID: process.env.NOTION_DB_ID,
+  },
 }
-
 export default nextConfig
